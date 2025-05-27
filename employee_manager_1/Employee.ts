06e1b96756bd9ca3bbaaf3cbe81f4dce6be3c823
Jithin <jithinka2323@gmail.com>
2025-05-27 10:59:45 +0530

    folder rename

--- employee_manager_1/Employee.ts
+++
@@ -1,7 +0,0 @@
-export default class Employee {
-    id: number;
-    name: string;
-    email: string;
-    createdAt: Date;
-    updatedAt: Date;
-}
--- employee_manager_NodeJS/test/services/employee.service.test.ts
+++ employee_manager_NodeJS/test/services/employee.service.test.ts
@@ -3,14 +3,17 @@ import EmployeeServices from "../../services/employee.services";
 import EmployeeRepository from "../../repositories/employee.repository";
 import Employee from "../../entities/employee.entity";
 import { when } from "jest-when";
+import DepartmentServices from "../../services/department.services";
 
 describe("EmployeeService", () => {
     let employeeRepository: MockProxy<EmployeeRepository>;
+    let departmentServices: MockProxy<DepartmentServices>;
     let employeeService: EmployeeServices;
 
     beforeEach(() => {
+        departmentServices = mock <DepartmentServices>();
         employeeRepository = mock<EmployeeRepository>();
-        employeeService = new EmployeeServices(employeeRepository);
+        employeeService = new EmployeeServices(employeeRepository, departmentServices);
     });
 
     describe("getEmployeeById", () => {
@@ -41,4 +44,35 @@ describe("EmployeeService", () => {
             expect(employeeRepository.findOneById).toHaveBeenCalledWith(12);
         });
     });
+
+    // describe("deleteEmployee", () => {
+    //     it("should delete user when user with proper id exists", async () => {
+    //         const mockEmployee = { id: 123, name: "Employee name" } as Employee;
+    //         when(employeeRepository.delete)
+    //             .calledWith(123)
+    //             .mockReturnValue(mockEmployee);
+
+    //         const user = await employeeService.deleteEmployee(mockEmployee.id);
+
+    //         expect(employeeRepository.delete).toHaveBeenCalledWith(123);
+    //         expect(user).toStrictEqual(mockEmployee);
+    //     });
+
+    //     it("should throw an error when user with provided id does not exist", async () => {
+    //         //Arrange
+    //         when(employeeRepository.delete)
+    //             .calledWith(12)
+    //             .mockReturnValue(null);
+
+    //         //Act
+    //         expect(employeeService.deleteEmployee(12)).rejects.toThrow(
+    //             "Employee not found"
+    //         );
+
+    //         //Assert
+    //         expect(employeeRepository.delete).toHaveBeenCalledWith(12);
+    //     });
+    // });
+
+
 });
